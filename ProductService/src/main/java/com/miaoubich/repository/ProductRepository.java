bd269997912e51f2969f33f8a6336e6b11e10ce7
Ali Bouzar <ali.bouzar@gmail.com>
2024-04-01 22:44:07 -0500

    fix error related to remote config server

--- ProductService/src/main/java/com/miaoubich/repository/ProductRepository.java
+++ ProductService/src/main/java/com/miaoubich/repository/ProductRepository.java
@@ -1,9 +1,11 @@
 package com.miaoubich.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.miaoubich.entity.Product;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Long>{
 
 }
